19f6909989e65a79c82f9b1ab2ff8a5c5932cfc6
Lukasz <lukaszkkoziel@gmail.com>
2020-08-07 09:57:14 +0900

    connected with blockchain, simple react added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
 import './App.css'
+import Navbar from './Navbar'
 
 
 class App extends Component {
 
   async componentWillMount() {
     await this.loadWeb3()
-    console.log(window.web3)
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const ehtBalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ ehtBalance })
+    console.log(this.state.ehtBalance)
   }
 
   async loadWeb3() {
@@ -23,18 +33,18 @@ class App extends Component {
     }
   }
 
+   constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      ehtBalance: '0'
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </nav>
+        <Navbar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
