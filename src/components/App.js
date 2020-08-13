b0212fe094d360bd7fd59dc18621a9b7108368ec
Lukasz <lukaszkkoziel@gmail.com>
2020-08-13 17:50:47 +0900

    changes to web3

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
-import './App.css'
 import Token from '../abis/Token.json'
-import EhtSwap from '../abis/EhtSwap.json'
+import EthSwap from '../abis/EhtSwap.json'
 import Navbar from './Navbar'
 import Main from './Main'
-
+import './App.css'
 
 class App extends Component {
 
@@ -16,36 +15,35 @@ class App extends Component {
 
   async loadBlockchainData() {
     const web3 = window.web3
+
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
-    const ehtBalance = await web3.eth.getBalance(this.state.account)
-    this.setState({ ehtBalance })
 
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ ethBalance })
 
-    const networkId = await web3.eth.net.getId()
+    // Load Token
+    const networkId =  await web3.eth.net.getId()
     const tokenData = Token.networks[networkId]
     if(tokenData) {
       const token = new web3.eth.Contract(Token.abi, tokenData.address)
       this.setState({ token })
       let tokenBalance = await token.methods.balanceOf(this.state.account).call()
       this.setState({ tokenBalance: tokenBalance.toString() })
-
     } else {
-      window.alert('Token contract not depoloyed to detected network')
+      window.alert('Token contract not deployed to detected network.')
     }
 
-
-
-    const ehtSwapData = EhtSwap.networks[networkId]
-    if(ehtSwapData) {
-      const ehtSwap = new web3.eth.Contract(EhtSwap.abi, ehtSwapData.address)
-      this.setState({ ehtSwap })
-      // this.setState({ ehtBalance: ehtBalance.toString() })
-
+    // Load EthSwap
+    const ethSwapData = EthSwap.networks[networkId]
+    if(ethSwapData) {
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
+      this.setState({ ethSwap })
     } else {
-      window.alert('EthSwap contract not depoloyed to detected network')
+      window.alert('EthSwap contract not deployed to detected network.')
     }
 
+    this.setState({ loading: false })
   }
 
   async loadWeb3() {
@@ -59,12 +57,9 @@ class App extends Component {
     else {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
-
-    this.setState({loading: false})
-
   }
 
-buyTokens = (etherAmount) => {
+  buyTokens = (etherAmount) => {
     this.setState({ loading: true })
     this.state.ethSwap.methods.buyTokens().send({ value: etherAmount, from: this.state.account }).on('transactionHash', (hash) => {
       this.setState({ loading: false })
@@ -73,20 +68,20 @@ buyTokens = (etherAmount) => {
 
   sellTokens = (tokenAmount) => {
     this.setState({ loading: true })
-    this.state.token.methods.approved(this.state.ehtSwap.address, tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
-    this.state.ehtSwap.methods.sellTokens(tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
-      this.setState({ loading: false })
+    this.state.token.methods.approve(this.state.ethSwap.address, tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+      this.state.ethSwap.methods.sellTokens(tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
       })
     })
   }
 
-   constructor(props) {
+  constructor(props) {
     super(props)
     this.state = {
       account: '',
       token: {},
-      ehtSwap: {},
-      ehtBalance: '0',
+      ethSwap: {},
+      ethBalance: '0',
       tokenBalance: '0',
       loading: true
     }
@@ -96,25 +91,31 @@ buyTokens = (etherAmount) => {
     let content
     if(this.state.loading) {
       content = <p id="loader" className="text-center">Loading...</p>
-    }
-    else {
+    } else {
       content = <Main
-        ehtBalance={this.state.ehtBalance}
+        ethBalance={this.state.ethBalance}
         tokenBalance={this.state.tokenBalance}
         buyTokens={this.buyTokens}
         sellTokens={this.sellTokens}
-        />
+      />
     }
 
     return (
       <div>
-        <Navbar account={this.state.account}/>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px'}}>
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
-              <Main />
-              {content}
+                <a
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                </a>
+
+                {content}
+
               </div>
             </main>
           </div>
@@ -123,6 +124,5 @@ buyTokens = (etherAmount) => {
     );
   }
 }
-
 
 export default App;
--- src/components/SellForm.js
+++ src/components/SellForm.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
 import tokenLogo from '../logoC.png'
-import ethLogo from '../Ethereum_logo_2014.svg.png'
-
+import ethLogo from '../logo.png'
 
 class SellForm extends Component {
-
   constructor(props) {
     super(props)
     this.state = {
@@ -15,71 +13,67 @@ class SellForm extends Component {
   render() {
     return (
       <form className="mb-3" onSubmit={(event) => {
-        event.preventDefault()
-        let etherAmount
-        etherAmount = this.input.value.toString()
-        etherAmount = window.web3.utils.toWei(etherAmount, 'Ether')
-        this.props.sellTokens(etherAmount)
-      }}>
-      <div>
-        <label className="float-left mr-1"><b>Input</b></label>
-          <span className="floot-right text-muted">
-             Balance: {this.props.tokenBalance}
+          event.preventDefault()
+          let etherAmount
+          etherAmount = this.input.value.toString()
+          etherAmount = window.web3.utils.toWei(etherAmount, 'Ether')
+          this.props.sellTokens(etherAmount)
+        }}>
+        <div>
+          <label className="float-left"><b>Input</b></label>
+          <span className="float-right text-muted">
+            Balance: {window.web3.utils.fromWei(this.props.tokenBalance, 'Ether')}
           </span>
-      </div>
-      <div className="input-group mb-4">
-        <input
-          type="text"
-          onChange={(event) => {
-            const tokenAmount = this.input.value.toString()
-            this.setState({
-              output: tokenAmount / 100
-            })
-          }}
-          ref={(input) => { this.input = input }}
-          className="form-control form-control-lg"
-          placeholder="0"
-          required />
-        <div className="input-group-append">
-          <div className="input-group-text">
-            <img src={tokenLogo} height='32' alt=""/>
-            &nbsp;&nbsp;&nbsp; WoB
+        </div>
+        <div className="input-group mb-4">
+          <input
+            type="text"
+            onChange={(event) => {
+              const tokenAmount = this.input.value.toString()
+              this.setState({
+                output: tokenAmount / 100
+              })
+            }}
+            ref={(input) => { this.input = input }}
+            className="form-control form-control-lg"
+            placeholder="0"
+            required />
+          <div className="input-group-append">
+            <div className="input-group-text">
+              <img src={tokenLogo} height='32' alt=""/>
+              &nbsp; DApp
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <label className="float-left"><b>Output</b></label>
-  <span className="float-right text-muted">
-    Balance: {this.props.ethBalance}
-  </span>
-
-
-
-      </div>
-      <div className="input-group mb-2">
-        <input
-          type="text"
-          className="form-control form-control-lg"
-          placeholder="0"
-          value={this.state.output}
-          disabled
-        />
-        <div className="input-group-append">
-          <div className="input-group-text">
-            <img src={ethLogo} height='32' alt=""/>
-            &nbsp; ETH
+        <div>
+          <label className="float-left"><b>Output</b></label>
+          <span className="float-right text-muted">
+            Balance: {window.web3.utils.fromWei(this.props.ethBalance, 'Ether')}
+          </span>
+        </div>
+        <div className="input-group mb-2">
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            placeholder="0"
+            value={this.state.output}
+            disabled
+          />
+          <div className="input-group-append">
+            <div className="input-group-text">
+              <img src={ethLogo} height='32' alt=""/>
+              &nbsp;&nbsp;&nbsp; ETH
+            </div>
           </div>
         </div>
-      </div>
-      <div className="mb-5">
-        <span className="float-left text-muted">Exchange Rate</span>
-        <span className="float-right text-muted">1 Wob = 100 ETH</span>
-      </div>
-      <button type="submit" className="btn btn-info btn-block btn-lg">EXCHANGE!!</button>
-    </form>
+        <div className="mb-5">
+          <span className="float-left text-muted">Exchange Rate</span>
+          <span className="float-right text-muted">100 DApp = 1 ETH</span>
+        </div>
+        <button type="submit" className="btn btn-primary btn-block btn-lg">SWAP!</button>
+      </form>
     );
   }
 }
-
 
 export default SellForm;
